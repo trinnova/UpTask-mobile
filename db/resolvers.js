ebc795861388e7743c83688379ba498102231688
Omar Garcia <omar.garcia@trinnova.mx>
2023-11-17 16:23:28 -0600

    Insertando usuarios en la BD

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,60 +1,38 @@
-
-const cursos = [
-    {
-        titulo: 'JS',
-        tecnologia: 'JS ECMA-E',
-    },
-
-    {
-        titulo: 'React',
-        tecnologia: 'React Native',
-    },
-
-    {
-        titulo: 'Node',
-        tecnologia: 'Node.js',
-    },
-
-    {
-        titulo: 'React Avanzado',
-        tecnologia: 'React Native, Apollo, GraphQL',
-    },
-];
-
-const tecnologias = [
-    {
-        nombre: 'HTML',
-        version: 'V5',
-    },
-
-    {
-        nombre: 'CSS',
-        version: 'V3',
-    },
-
-    {
-        nombre: 'SASS',
-        version: 'V20.1',
-    },
-
-    {
-        nombre: 'BOOTSTRAP',
-        version: 'V5',
-    },
-];
+const Usuario = require('../models/Usuario');
 
 // Resolver: son funciones que son responsables de retornar los valores que existen en los 
 const resolvers = {
     Query: {
-        obtenerCursos: () => cursos,
 
-        obtenerTecnologia: () => tecnologias
     },
 
     Mutation: {
-        crearUsuario: (_, {input}) => {
-            console.log(input);
-            console.log('Creando usuario...');
+        crearUsuario: async (_, {input}) => {
+            const { email, password } = input;
+
+            // findOne = busca 1 registro
+            const existeUsuario = await Usuario.findOne({ email });
+
+            // Si el usuario existe
+            if (existeUsuario)
+            {
+                throw new Error('El usuario ya esta registrado');
+            }
+
+            try
+            {
+                const nuevoUsuario = new Usuario(input);
+                console.log(nuevoUsuario);
+
+                // Guardar en la BD
+                nuevoUsuario.save();
+
+                return "Usuario creado correctamente";
+            }
+            catch (error)
+            {
+                console.error(error);
+            }
         }
     }
 };
