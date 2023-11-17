a7e7e3ff66d0ab45905902a73a76d9eb158f90cb
Omar Garcia <omar.garcia@trinnova.mx>
2023-11-17 10:11:03 -0600

    Creando un QUERY y un RESOLVER

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
 const { ApolloServer, gql } = require('apollo-server');
 
 const typeDefs = gql`
+
+    type Curso {
+        titulo: String,
+        teconologia: String
+    }
+
     type Query {
-        
+        obtenerCursos: [Curso]
     }
 `;
 
-const server = new ApolloServer();
+const cursos = [
+    {
+        titulo: 'JS',
+        tecnologia: 'JS ECMA-E',
+    },
+
+    {
+        titulo: 'React',
+        tecnologia: 'React Native',
+    },
+
+    {
+        titulo: 'Node',
+        tecnologia: 'Node.js',
+    },
+
+    {
+        titulo: 'React Avanzado',
+        tecnologia: 'React Native, Apollo, GraphQL',
+    },
+]
+
+// Resolver: son funciones que son responsables de retornar los valores que existen en los 
+const resolvers = {
+    Query: {
+        obtenerCursos: () => cursos
+    }
+}
+
+const server = new ApolloServer( {typeDefs, resolvers} );
 
 server.listen().then( ({url}) => {
     console.log(`Servidor listo en la URL: ${url}`);
