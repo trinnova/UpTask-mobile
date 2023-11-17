d9be62681d8db391999257e92b986eaf13b6aade
Omar Garcia <omar.garcia@trinnova.mx>
2023-11-17 11:39:25 -0600

    Conectando a una BD

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+require('dotenv').config({path: 'variables.env'});
+
+const conectarDB = async () => {
+    try {
+        await mongoose.connect(process.env.DB_MONGO, {
+            useNreUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+        });
+
+        console.log('DB Conectada');
+    } catch (e) {
+        console.log('Hubo un error');
+        console.log(e);
+        process.exit(1); // Deneter la app
+    }
+}
+
+module.exports = conectarDB;
--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const { ApolloServer } = require('apollo-server');
 
 // Importando los schemas
 const typeDefs = require('./db/schema');
-
 const resolvers = require('./db/resolvers');
+const conectarDB = require('./config/db');
+
+// Conectar a la BD
+conectarDB();
+
 
 const server = new ApolloServer( {typeDefs, resolvers} );
 
